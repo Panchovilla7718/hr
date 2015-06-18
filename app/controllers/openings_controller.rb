b7e365e8fd2bbdb47f4dc854e1317265dc642ce0
David Contreras <david.contrerasrdz@gmail.com>
2015-06-18 03:00:14 +0000

    logic for opening page completed

--- app/controllers/openings_controller.rb
+++ app/controllers/openings_controller.rb
@@ -1,8 +1,14 @@
 class OpeningsController < ApplicationController
     def index
+        @openings = Opening.all
     end
     
     def openings
+        @openings = Opening.all
+    end
+    
+    def new
+    
     end
     
 end
