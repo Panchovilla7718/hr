cec025cd797243dab8a02cb4555af495ac11e903
David Contreras <david.contrerasrdz@gmail.com>
2015-06-24 16:23:05 +0000

    Edit, Update logic created. Flash msg not created yet

--- app/controllers/openings_controller.rb
+++ app/controllers/openings_controller.rb
@@ -7,8 +7,49 @@ class OpeningsController < ApplicationController
         @openings = Opening.all
     end
     
-    def new
+    def show
+        @openings = Opening.find(params[:id])    
+    end
     
+    
+    def new
+        @openings = Opening.new
+    end
+    
+    def create
+        @opening = Opening.create(openings_param)
+        @opening.supervisor = Supervisor.find(1)
+        
+        if @opening.save
+            flash[:success] = "Request created sucessfully!"
+            redirect_to openings_path
+            
+        else
+            render :new
+        end
+    end
+    
+    def edit
+        @opening = Opening.edit(params[:id])
+    end
+    
+    def update
+       @opening = Opening.find(params[:id])
+       
+        if @opening.update(openings_param)
+            flash[:success] = "Request created sucessfully!"
+            redirect_to openings_path
+        else
+            render :edit
+        end
+    end
+    
+    private 
+    
+    def openings_param
+        params.require(:opening).permit(:position, :replacing, :emp, :reason, :action, :shift, :dept, :schedule, :status)
     end
     
 end
+
+
